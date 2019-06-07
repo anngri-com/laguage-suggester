8c71334ef1ec1e6d76fffd60ebfd8382b93d6ebd
Anna <anna.gritsay@gmail.com>
2019-06-07 12:23:54 -0700

    added language suggestion

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,36 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-
+  
   var activity = $("input[name='activity']:checked").val();
   var place = $("input[name='place']:checked").val();
   var toys = $("input[name='toys']:checked").val();
   var workout = $("input[name='workout']:checked").val();  
 
-
-
-  if (activity === "instagram" && activity === "outdoors" || activity === "cars" && activity === "yes") {
+  if (activity === "instagram" && place === "outdoors") {
     $("#your-language").text("Ruby");
-  } else if (activity === "books" && activity === "room" || activity === "hummer" && activity === "what") {
+  } else if (activity === "instagram" && place === "room") {
     $("#your-language").text("Rust");
-  } else (activity === "games" && activity === "kitchen" || activity === "joystick" && activity === "no") {
+  } else if (activity === "instagram" && place === "kitchen") {
     $("#your-language").text("Python");
+  }
 
-  if (place === "instagram" && place === "outdoors" || place === "cars" && place === "yes") {
+  if (activity === "books" && toys === "cars") {
     $("#your-language").text("Ruby");
-  } else if (place === "books" && place === "room" || place === "hummer" && place === "what") {
+  } else if (activity === "books" && toys === "hummer") {
     $("#your-language").text("Rust");
-  } else (place === "games" && place === "kitchen" || place === "joystick" && plce === "no") {
+  } else if (activity === "books" && place === "joystick") {
     $("#your-language").text("Python");
-    
-    event.preventDefault();
+  }
+
+  if (activity === "games" && workout === "no") {
+    $("#your-language").text("Ruby");
+  } else if (activity === "games" && workout === "what") {
+    $("#your-language").text("Rust");
+  } else if (activity === "games" && workout === "yes") {
+    $("#your-language").text("Python");
+  }
+  
+  event.preventDefault();
+  
   });
 });
-
-
-
-
-
-
-
-
-
-// var a = parseInt($("input#sideA").val());
-//     var b = parseInt($("input#sideB").val());
-//     var c = parseInt($("input#sideC").val());
-
-//     if ( a === b && a === c) {
-//       //return an equilateral
-//       $("#type").text("Equilateral"); 
-//     }
-//     else if ( a + b <= c 
-//             || c + b <= a 
-//             || a + c <= b) {
-//       // return NOT a triangle
-//       $("#type").text("NOT a triangle");
-//     }
-//     else if (a === b && a !== c || 
-//              b === c && b !== a || 
-//              c === a && c !== b) {
-//       // return Isosceles
-//       $("#type").text("Isosceles");
-//     }
-//     else {
-//       // return Scalene
-//       $("#type").text("Scalene");
-//     }
-    
